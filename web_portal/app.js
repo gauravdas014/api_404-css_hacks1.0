f9ae65e6057852807cc9c16079cc76438762e0ac
Gaurav Das <gauravdas014@gmail.com>
2021-02-12 19:00:26 +0530

    add initial project files

--- web_portal/app.js
+++ web_portal/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const viewRouter = require('./routes/viewRoutes');
-const authRouter = require('./routes/authRoutes');
+// const viewRouter = require('./routes/viewRoutes');
+// const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.render('portal/homepage');
 });
 
-app.use('/', viewRouter);
-app.use('/auth', authRouter);
+// app.use('/', viewRouter);
+// app.use('/auth', authRouter);
 
 module.exports = app;
