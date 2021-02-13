0e92e299f21b7a4ce3ddf9a3c6f51f9b46d68e89
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-02-12 22:52:25 -0500

    Firebase login completed

--- androd_app/app/src/main/java/com/example/nirog/Authentication/LoginFragment.java
+++ androd_app/app/src/main/java/com/example/nirog/Authentication/LoginFragment.java
@@ -2,60 +2,141 @@ package com.example.nirog.Authentication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.nirog.R;
 import com.example.nirog.databinding.FragmentLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginFragment extends Fragment {
 
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     //binding
     private FragmentLoginBinding binding;
 
+    private SignupFragment signupFragment;
+    private ForgotPasswordFragment forgotPasswordFragment;
+    private FirebaseAuth mAuth;
 
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        signupFragment = new SignupFragment();
+        forgotPasswordFragment = new ForgotPasswordFragment();
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(signupFragment);
+            }
+        });
+
+        binding.forgotPasswordFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(forgotPasswordFragment);
+            }
+        });
+
+        binding.Loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.progresslogin.setVisibility(View.VISIBLE);
+                checkdata();
+            }
+        });
+
+        binding.email.addTextChangedListener(loginTextWatcher);
+        binding.password.addTextChangedListener(loginTextWatcher);
+
+
         return binding.getRoot();
     }
 
+    private void checkdata() {
+        String E = binding.email.getText().toString();
+        String P = binding.password.getText().toString();
+        if(E.length()==0){
+            binding.emailTxtiplayout.setError("Required Field");
+            binding.progresslogin.setVisibility(View.INVISIBLE);
+        }
+        else if(P.length()==0) {
+            binding.passwordTxtiplayout.setError("Required Field");
+            binding.progresslogin.setVisibility(View.INVISIBLE);
+        }
+        else{
+            logincheck();
+        }
+    }
+
+    private void logincheck() {
+        mAuth.signInWithEmailAndPassword(binding.email.getText().toString(),binding.password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            binding.progresslogin.setVisibility(View.INVISIBLE);
+                            Toast.makeText(getActivity(),"Login Successfull",Toast.LENGTH_SHORT).show();
+                            //setFragmentNoBackStack(new BottomNavFragment());
+                        }
+                        else{
+                            binding.progresslogin.setVisibility(View.INVISIBLE);
+                            Toast.makeText(getActivity(),"Error in Login!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(),""+e.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container,fragment);
+        fragmentTransaction.addToBackStack(null).commit();
+    }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String emailInput = binding.email.getText().toString().trim();
+            String passwordInput = binding.password.getText().toString().trim();
+
+            //enabling or disabling the button if there is no any text
+            binding.Loginbtn.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     @Override
     public void onDestroyView() {
