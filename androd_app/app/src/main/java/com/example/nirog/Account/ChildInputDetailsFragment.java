4b7853c7a1282a5ec84ca2240c9b1fcbeae0bad4
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-02-13 10:12:25 +0530

    Change Log -
    Child form fill up details fragment added

---
+++ androd_app/app/src/main/java/com/example/nirog/Account/ChildInputDetailsFragment.java
@@ -0,0 +1,57 @@
+package com.example.nirog.Account;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nirog.R;
+import com.example.nirog.databinding.FragmentChildInputDetailsBinding;
+
+
+public class ChildInputDetailsFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    //view binding
+    private FragmentChildInputDetailsBinding binding;
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public ChildInputDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ChildInputDetailsFragment newInstance(String param1, String param2) {
+        ChildInputDetailsFragment fragment = new ChildInputDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentChildInputDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
