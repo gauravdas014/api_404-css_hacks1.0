1844cdb11ad544dbd1936c9097355a1d1296ea1b
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-02-13 22:21:59 +0530

    Doctor list adapter set up complete

---
+++ androd_app/app/src/main/java/com/example/nirog/MainDestinations/Hospital/DoctosListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.nirog.MainDestinations.Hospital;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.nirog.R;
+import com.example.nirog.data.model.DoctorDetails;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.List;
+
+public class DoctosListAdapter extends RecyclerView.Adapter<DoctosListAdapter.DoctorViewHolder> {
+
+    Context context;
+    List<DoctorDetails> doctorDetailsList;
+
+    public DoctosListAdapter(Context context, List<DoctorDetails> doctorDetailsList) {
+        this.context = context;
+        this.doctorDetailsList = doctorDetailsList;
+    }
+
+    @NonNull
+    @Override
+    public DoctorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DoctorViewHolder(LayoutInflater.from(context).inflate(R.layout.doctors_horizontalrow_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DoctorViewHolder holder, int position) {
+        DoctorDetails currentItem = doctorDetailsList.get(position);
+        holder.doctorName.setText(currentItem.getName());
+        holder.doctorDegree.setText(currentItem.getSpecialization());
+        holder.doctorEmail.setText(currentItem.getEmail());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return doctorDetailsList.size();
+    }
+
+    public class DoctorViewHolder extends RecyclerView.ViewHolder {
+
+        ShapeableImageView imageView;
+        TextView doctorName, doctorDegree, doctorEmail;
+
+        public DoctorViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //setting the hooks
+            imageView = itemView.findViewById(R.id.doctor_image);
+            doctorName = itemView.findViewById(R.id.doctor_name);
+            doctorDegree = itemView.findViewById(R.id.doctor_degree);
+            doctorEmail = itemView.findViewById(R.id.doctor_email);
+        }
+    }
+}
