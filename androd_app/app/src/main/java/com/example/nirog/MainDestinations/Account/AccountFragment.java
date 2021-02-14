ddb0af4f6db9355a87d2fabee44bb5f3bbdcb30d
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-02-14 08:56:35 +0530

    AccountFragment fragment added

---
+++ androd_app/app/src/main/java/com/example/nirog/MainDestinations/Account/AccountFragment.java
@@ -0,0 +1,58 @@
+package com.example.nirog.MainDestinations.Account;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nirog.R;
+import com.example.nirog.databinding.FragmentAccountBinding;
+
+
+public class AccountFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    //view binding
+    private FragmentAccountBinding binding;
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AccountFragment newInstance(String param1, String param2) {
+        AccountFragment fragment = new AccountFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
