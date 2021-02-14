d8c74febb7268ab6dd8840ec0e9d84665d9e196a
Gaurav Das <gauravdas014@gmail.com>
2021-02-14 16:23:29 +0530

    add dashboard pages

--- web_portal/routes/hospitalRoutes.js
+++ web_portal/routes/hospitalRoutes.js
@@ -6,4 +6,17 @@ router.route('/all').get(hospitalController.getAllHospitals);
 router.route('/:hospitalId').get(hospitalController.getHospital);
 router.route('/:hospitalId').post(hospitalController.editHospitalDetails);
 
+// For Portal
+router
+  .route('/update/info/:hospitalId')
+  .post(hospitalController.updateDetailsInPortal);
+
+router
+  .route('/add/doctor/:hospitalId')
+  .post(hospitalController.addDoctorForPortal);
+
+router
+  .route('/add/vaccine/:hospitalId')
+  .post(hospitalController.addVaccineForPortal);
+
 module.exports = router;
